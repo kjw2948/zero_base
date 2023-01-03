05b0436cf3e6b0fe70031e67df18c8cacf981641
kjw2948 <kjw970313@cau.ac.kr>
2023-01-03 20:55:11 +0900

    zero_base problem 4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public void gugudan() {
         String s1;
@@ -67,8 +68,37 @@ public class Main {
             System.out.println("입장료 : " + ticket_price );
         }
     }
+    public void creatId(){
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        int s_num;
+
+        System.out.print("출생년도를 입력해주세요. : ");
+        int year = in.nextInt();
+        System.out.print("출생월을 입력해주세요. : ");
+        int month = in.nextInt();
+        System.out.print("출생일을 입력해주세요. : ");
+        int day = in.nextInt();
+        System.out.print("성별을 입력해주세요. : ");
+        String sex = in.next();
+        char s = sex.charAt(0);
+
+        if(s == 'm'){
+            s_num = 3;
+        }
+        else {
+            s_num = 4;
+        }
+        int random_number;
+        System.out.printf("%d%d%d-%d",year,month,day,s_num);
+        for (int i = 0; i < 6; i++) {
+            random_number = rnd.nextInt(10);
+            System.out.print(random_number);
+        }
+    }
+
     public static void main(String[] args) {
         Main s1 = new Main();
-        s1.ticket();
+        s1.creatId();
     }
 }
