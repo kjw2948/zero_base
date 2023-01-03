ea26545e9b5043a214bafd1487b2c378d93e6c96
kjw2948 <kjw970313@cau.ac.kr>
2023-01-03 20:00:57 +0900

    zero_base problem 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public void gugudan() {
         String s1;
@@ -23,9 +24,22 @@ public class Main {
             System.out.println(" ");
         }
     }
-
+    public void cashBack(){
+        Scanner in = new Scanner(System.in);
+        var cashback = 0;
+        System.out.print("결제 금액을 입력해주세요.(금액) : ");
+        int cash = in.nextInt();
+        if (cash>3000) {
+            cashback = 300;
+        }
+        else {
+            cashback = cash/10;
+            cashback = (int) (Math.floor(cashback / 100)*100);
+        }
+        System.out.println("결제 금액은 "+ cash +"원이고, 캐시백은 " +cashback + "원 입니다.");
+    }
     public static void main(String[] args) {
         Main s1 = new Main();
-        s1.gugudan();
+        s1.cashBack();
     }
 }
